4bfc892c6859662739b47b66cdba0d91977f50ce
ahsan094 <ahsan.sajjad@softoo.co>
2020-12-23 11:55:21 +0500

    Initial commit

---
+++ ExcerciseTracker/Prediction + Poses.swift
@@ -0,0 +1,12 @@
+//
+//  Prediction + Poses.swift
+//  ExcerciseTracker
+//
+//  Created by Ahsan on 14/12/2020.
+//
+
+import Foundation
+import Vision
+import CoreML
+
+
--- ExcerciseTracker/ViewController.swift
+++ ExcerciseTracker/ViewController.swift
@@ -6,14 +6,260 @@
 //
 
 import UIKit
+import Vision
+import CoreML
+import Foundation
+import AVFoundation
+
+
+@available(iOS 14.0,*)
+class Predicator  {
+    let fitnessClassifier = ExcerciseModel_1()
+    
+    let humanBodyPoseRequest = VNDetectHumanBodyPoseRequest()
+    
+    
+    var poseWindow: [VNRecognizedTextObservation?] = []
+    
+    init() {
+        poseWindow.reserveCapacity(60)
+    }
+    
+    
+    
+}
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var lblPersonStatus: UILabel!
+    @IBOutlet weak var lblPoseName: UILabel!
+    private let detectPlayerRequest = VNDetectHumanBodyPoseRequest()
+    private let videoDataOutputQueue = DispatchQueue(label: "CameraFeedDataOutput", qos: .userInitiated,
+                                                     attributes: [], autoreleaseFrequency: .workItem)
+    
+    private var cameraFeedView: CameraFeedView!
+    private var cameraFeedSession: AVCaptureSession?
+
+    // Video file playback management
+    private var videoRenderView: VideoRenderView!
+    private var playerItemOutput: AVPlayerItemVideoOutput?
+    private var displayLink: CADisplayLink?
+    private let videoFileReadingQueue = DispatchQueue(label: "VideoFileReading", qos: .userInteractive)
+    private var videoFileBufferOrientation = CGImagePropertyOrientation.up
+    private var videoFileFrameDuration = CMTime.invalid
+    
+    var playerDetected = Bool()
+    
+    private let playerBoundingBox = BoundingBoxView()
+    
+    private let bodyPoseDetectionMinConfidence: VNConfidence = 0.6
+    private let trajectoryDetectionMinConfidence: VNConfidence = 0.9
+    private let bodyPoseRecognizedPointMinConfidence: VNConfidence = 0.1
+    
+    private let jointSegmentView = JointSegmentView()
+    
+    var poseObservations = [VNHumanBodyPoseObservation]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        do{
+        try setupAVSession()
+        }catch{
+            AppError.display(error, inViewController: self)
+        }
+    }
+
+    func setupAVSession() throws {
+        // Create device discovery session for a wide angle camera
+        let wideAngle = AVCaptureDevice.DeviceType.builtInWideAngleCamera
+        let discoverySession = AVCaptureDevice.DiscoverySession(deviceTypes: [wideAngle], mediaType: .video, position: .unspecified)
+        
+        // Select a video device, make an input
+        guard let videoDevice = discoverySession.devices.first else {
+            throw AppError.captureSessionSetup(reason: "Could not find a wide angle camera device.")
+        }
+        
+        guard let deviceInput = try? AVCaptureDeviceInput(device: videoDevice) else {
+            throw AppError.captureSessionSetup(reason: "Could not create video device input.")
+        }
+        
+        let session = AVCaptureSession()
+        session.beginConfiguration()
+        // We prefer a 1080p video capture but if camera cannot provide it then fall back to highest possible quality
+        if videoDevice.supportsSessionPreset(.hd1920x1080) {
+            session.sessionPreset = .hd1920x1080
+        } else {
+            session.sessionPreset = .high
+        }
+        
+        // Add a video input
+        guard session.canAddInput(deviceInput) else {
+            throw AppError.captureSessionSetup(reason: "Could not add video device input to the session")
+        }
+        session.addInput(deviceInput)
+        
+        let dataOutput = AVCaptureVideoDataOutput()
+        if session.canAddOutput(dataOutput) {
+            session.addOutput(dataOutput)
+            // Add a video data output
+            dataOutput.alwaysDiscardsLateVideoFrames = true
+            dataOutput.videoSettings = [
+                String(kCVPixelBufferPixelFormatTypeKey): Int(kCVPixelFormatType_420YpCbCr8BiPlanarFullRange)
+            ]
+            dataOutput.setSampleBufferDelegate(self, queue: videoDataOutputQueue)
+        } else {
+            throw AppError.captureSessionSetup(reason: "Could not add video data output to the session")
+        }
+        let captureConnection = dataOutput.connection(with: .video)
+        captureConnection?.preferredVideoStabilizationMode = .standard
+        // Always process the frames
+        captureConnection?.isEnabled = true
+        session.commitConfiguration()
+        cameraFeedSession = session
+        
+        // Get the interface orientaion from window scene to set proper video orientation on capture connection.
+        let videoOrientation: AVCaptureVideoOrientation
+        switch view.window?.windowScene?.interfaceOrientation {
+        case .landscapeRight:
+            videoOrientation = .landscapeRight
+        default:
+            videoOrientation = .portrait
+        }
+        
+        // Create and setup video feed view
+        cameraFeedView = CameraFeedView(frame: view.bounds, session: session, videoOrientation: videoOrientation)
+        setupVideoOutputView(cameraFeedView)
+        cameraFeedSession?.startRunning()
+        lblPersonStatus.text = "Searching for person....."
+        //setUIElements()
+    }
+    
+    func setupVideoOutputView(_ videoOutputView: UIView) {
+        videoOutputView.translatesAutoresizingMaskIntoConstraints = false
+        videoOutputView.backgroundColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        view.insertSubview(videoOutputView, at: 0)
+        NSLayoutConstraint.activate([
+            videoOutputView.leftAnchor.constraint(equalTo: view.leftAnchor),
+            videoOutputView.rightAnchor.constraint(equalTo: view.rightAnchor),
+            videoOutputView.topAnchor.constraint(equalTo: view.topAnchor),
+            videoOutputView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
     }
 
 
+
+    
+
 }
 
+extension ViewController: AVCaptureVideoDataOutputSampleBufferDelegate {
+    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+    
+        let visionHandler = VNImageRequestHandler(cmSampleBuffer: sampleBuffer, orientation: .up, options: [:])
+        
+        
+        
+        do {
+            try visionHandler.perform([detectPlayerRequest])
+            if let result = detectPlayerRequest.results?.first {
+                let box = humanBoundingBox(for: result)
+                let boxView = playerBoundingBox
+                DispatchQueue.main.async {
+                    let inset: CGFloat = -20.0
+                    let viewRect = self.viewRectForVisionRect(box).insetBy(dx: inset, dy: inset)
+                    
+                    self.updateBoundingBox(boxView, withRect: viewRect)
+                    if !self.playerDetected && !boxView.isHidden {
+                        
+                        //self.gameStatusLabel.alpha = 0
+                        self.playerDetected = true
+                        self.playerBoundingBox.perform(transition: .fadeOut, duration: 1.0)
+                        self.lblPersonStatus.text = "Person Detected"
+                        print("Player Detected......")
+                    }
+                }
+            }
+        } catch {
+            AppError.display(error, inViewController: self)
+        }
+
+    }
+    
+    
+    
+    func updateBoundingBox(_ boundingBox: BoundingBoxView, withRect rect: CGRect?) {
+        // Update the frame for player bounding box
+        boundingBox.frame = rect ?? .zero
+        boundingBox.perform(transition: (rect == nil ? .fadeOut : .fadeIn), duration: 0.1)
+    }
+
+    func humanBoundingBox(for observation: VNHumanBodyPoseObservation) -> CGRect {
+        var box = CGRect.zero
+        var normalizedBoundingBox = CGRect.null
+        // Process body points only if the confidence is high.
+        guard observation.confidence > bodyPoseDetectionMinConfidence, let points = try? observation.recognizedPoints(forGroupKey: .all) else {
+            return box
+        }
+        // Only use point if human pose joint was detected reliably.
+        for (_, point) in points where point.confidence > bodyPoseRecognizedPointMinConfidence {
+            normalizedBoundingBox = normalizedBoundingBox.union(CGRect(origin: point.location, size: .zero))
+        }
+        if !normalizedBoundingBox.isNull {
+            box = normalizedBoundingBox
+        }
+        // Fetch body joints from the observation and overlay them on the player.
+        let joints = getBodyJointsFor(observation: observation)
+        DispatchQueue.main.async {
+            self.jointSegmentView.joints = joints
+        }
+        // Store the body pose observation in playerStats when the game is in TrackThrowsState.
+        // We will use these observations for action classification once the throw is complete.
+        
+        if playerDetected {
+          
+            
+            if poseObservations.count >= GameConstants.maxPoseObservations {
+                poseObservations.removeFirst()
+            }
+            poseObservations.append(observation)
+           
+            if let actionClassifier = try? ExcerciseModel_1(configuration: MLModelConfiguration()),
+                  let poseMultiArray = prepareInputWithObservations(poseObservations),
+                  let predictions = try? actionClassifier.prediction(poses: poseMultiArray)
+            {
+                DispatchQueue.main.async {
+                    if predictions.label == "Squat"{
+                        self.lblPoseName.text = "Other"
+                    }else{
+                    self.lblPoseName.text = predictions.label
+                    }
+                }
+               
+            }
+            
+           
+            }
+            
+
+            
+        
+        
+        return box
+    }
+    
+    
+    func viewRectForVisionRect(_ visionRect: CGRect) -> CGRect {
+        let flippedRect = visionRect.applying(CGAffineTransform.verticalFlip)
+        let viewRect: CGRect
+        if cameraFeedSession != nil {
+            viewRect = cameraFeedView.viewRectConverted(fromNormalizedContentsRect: flippedRect)
+        } else {
+            viewRect = videoRenderView.viewRectConverted(fromNormalizedContentsRect: flippedRect)
+        }
+        return viewRect
+    }
+    
+}
